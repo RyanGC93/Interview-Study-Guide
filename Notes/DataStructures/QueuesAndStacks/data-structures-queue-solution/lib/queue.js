e437014f0dc274044d0c9d85d4237c99de26d23a
Ryan Conk <ryangconk@gmail.com>
2022-04-22 18:55:31 -0600

    added queue

--- Notes/DataStructures/QueuesAndStacks/data-structures-queue-solution/lib/queue.js
+++ Notes/DataStructures/QueuesAndStacks/data-structures-queue-solution/lib/queue.js
@@ -13,7 +13,7 @@
 // Constraints:
 // ------------
 //
-// Make sure the time and space complexity of each is equivalent to those 
+// Make sure the time and space complexity of each is equivalent to those
 // in the table provided in the Time and Space Complexity Analysis section
 // of your Stack reading!
 //
@@ -22,11 +22,48 @@
 // -----------
 
 class Node {
-
+  constructor(val) {
+    this.value = val;
+    this.next = null;
+  }
 }
 
 class Queue {
+  constructor() {
+    this.front = null;
+    this.back = null;
+    this.length = 0;
+  }
 
+  enqueue(val) {
+    let newNode = new Node(val);
+    if (!this.length) {
+      this.front = newNode;
+      this.back = newNode;
+      this.length++;
+      return this.length;
+    } else {
+      this.back.next = newNode;
+      this.back = newNode;
+      this.length++;
+      return this.length;
+    }
+  }
+  dequeue() {
+    let popped = this.front;
+    if (!this.front) return null;
+    if (this.back === this.front) {
+      this.back = null;
+      this.front = null;
+    } else {
+      this.front = this.front.next;
+    }
+    this.length--;
+    return popped.value;
+  }
+  size() {
+    return this.length;
+  }
 }
 
 exports.Node = Node;
