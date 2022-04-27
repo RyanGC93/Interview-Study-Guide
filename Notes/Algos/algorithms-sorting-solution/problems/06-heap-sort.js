9059a698e8b19ee17f45d845a243b93829db35b5
Ryan Conk <65003807+RyanGC93@users.noreply.github.com>
2022-04-27 11:06:52 -0600

    add heap sort

--- Notes/Algos/algorithms-sorting-solution/problems/06-heap-sort.js
+++ Notes/Algos/algorithms-sorting-solution/problems/06-heap-sort.js
@@ -1,8 +1,41 @@
-// Implement Heap Sort
+function swap(array, i, j) {
+  var temp = array[i];
+  array[i] = array[j];
+  array[j] = temp;
+}
+
+function heapify(array, n, i) {
+  let largest = i;
+  let l = 2 * i + 1;
+  let r = 2 * i + 2;
+  if (l < n && array[l] > array[largest]) {
+    largest = l;
+  }
+  if (r < n && array[r] > array[largest]) {
+    largest = r;
+  }
+  if (largest !== i) {
+    swap(array, i, largest);
+    heapify(array, n, largest);
+  }
+}
+
+
+
 
 function heapSort(array) {
-    // your code here
+  // Build the heap
+  for (let i = Math.floor(array.length / 2); i >= 0; i--) {
+    heapify(array, array.length, i);
   }
+
+  // Sort the heap
+  for (let i = array.length - 1; i >= 0; i--) {
+    swap(array, 0, i);
+    heapify(array, i, 0);
+  }
+  return array;
+}
   
   
   module.exports = {
